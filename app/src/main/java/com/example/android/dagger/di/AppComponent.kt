f5140bade1c2162b9101536a4b583fae55fb15d2
Adolfo Bulfoni <adolfo.bulfoni@gmail.com>
2019-12-15 22:06:58 +0100

    codelab (minus tests)

---
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -0,0 +1,29 @@
+package com.example.android.dagger.di
+
+import android.content.Context
+import com.example.android.dagger.login.LoginComponent
+import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.registration.RegistrationComponent
+import com.example.android.dagger.settings.SettingsActivity
+import com.example.android.dagger.user.UserComponent
+import com.example.android.dagger.user.UserManager
+import dagger.BindsInstance
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [StorageModule::class, AppSubComponents::class])
+interface AppComponent {
+
+    @Component.Factory
+    interface Factory {
+        // with @BindsInstance, the context passed in will be available in the graph
+        // since it is passed when AppComponent is created, it will be a unique instance
+        fun create(@BindsInstance context: Context) : AppComponent
+    }
+
+    fun registrationComponent(): RegistrationComponent.Factory
+    fun loginComponent(): LoginComponent.Factory
+
+    fun userManager(): UserManager
+}
